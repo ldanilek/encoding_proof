a2b90c8e198b565bb02f46903f39a1066c5d6d5a
Lee Danilek <lee.danilek@gmail.edu>
2021-05-13 20:06:05 -0700

    first code that compiles

---
+++ Prefix.v
@@ -0,0 +1,16 @@
+Require Import EqNat.
+Require Import PeanoNat.
+Require Import String.
+Require Import BinNums.
+Require Import List.
+Require Import Compare_dec.
+Require Import Omega.
+Set Implicit Arguments.
+
+Section PREFIX.
+
+Definition V: Type := list bool.
+
+
+
+End PREFIX.
