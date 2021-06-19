82390bed7e32cc15eaf157fd725670d9736e6713
yichaolemon <yclemontree0@gmail.com>
2021-06-18 22:08:50 -0700

    worked on the inv proof

--- Prefix.v
+++ Prefix.v
@@ -67,9 +67,27 @@ intro. rewrite H. rewrite firstn_O. rewrite app_nil_r. trivial.
 omega.
 Qed.
 
+Lemma is_prefix_refl: forall b, is_prefix b b.
+Proof.
+intros. apply is_prefix_app. exists nil. apply app_nil_r.
+Qed.
+
+Lemma is_prefix_nil: forall b, is_prefix nil b.
+Proof.
+intros. autounfold. simpl. easy.
+Qed.
+
+End PREFIX.
+
 Section CODE.
 
+(* the type being encoded. e.g. letters *)
 Variable V: Type.
+
+(* V has at least two possible values *)
+Variable v01 v02: V.
+Hypothesis v_diff:
+  v01 <> v02.
 
 Hypothesis v_dec:
   forall (v1 v2: V), {v1 = v2} + {v1 <> v2}.
@@ -90,17 +108,35 @@ you can't have one code be a prefix of another.
 Hypothesis is_prefix_code:
   forall v1 v2, is_prefix (c v1) (c v2) -> v1 = v2.
 
+Lemma nonempty_code: forall v, c v <> nil.
+Proof.
+intros. intro.
+cut (exists v1, v1 <> v).
+intro. destruct H0.
+assert (NIL := is_prefix_nil (c x)).
+rewrite <- H in NIL.
+apply is_prefix_code in NIL.
+apply H0. auto.
+destruct (v_dec v v01).
+exists v02. intro. subst v. rewrite H0 in v_diff. contradiction.
+exists v01. intro. rewrite H0 in n. contradiction.
+Qed.
+
 Definition encode (vs: list V): B :=
   flat_map c vs.
 
-Fixpoint decode_helper (b unmatched: B): option (list V) :=
+(* decodes unmatched++b into list of values *)
+Fixpoint decode_helper (unmatched b: B) {struct b}: option (list V) :=
   match b with
-  | nil => Some nil
+  | nil => match unmatched with
+    | nil => Some nil
+    | _ => None
+    end
   | f::r =>
     let pref := unmatched ++ (f::nil) in
       match d pref with
-      | None => decode_helper r pref
-      | Some v => match decode_helper r nil with
+      | None => decode_helper pref r
+      | Some v => match decode_helper nil r with
         | None => None
         | Some vs => Some (v :: vs)
         end
@@ -108,32 +144,43 @@ Fixpoint decode_helper (b unmatched: B): option (list V) :=
   end.
 
 Definition decode (b: B): option (list V) :=
-  decode_helper b nil.
+  decode_helper nil b.
 
-(* lol this is false *)
-Lemma flat_map_nil: 
-  forall A B (f: A->list B) x,
-  flat_map f x = nil -> x = nil.
+Lemma encode_not_nil:
+  forall x, encode x = nil -> x = nil.
 Proof.
 induction x; simpl; intros.
 trivial.
 apply app_eq_nil in H. destruct H.
+apply nonempty_code in H.
+contradiction.
 Qed.
+
+Lemma decode_helper_inv:
+  forall b unmatched vs, decode_helper unmatched b = Some vs 
+  <-> encode vs = unmatched ++ b.
+Proof.
+induction b; induction unmatched; simpl; intros.
+- split; intros. injection H; intros; subst. simpl. trivial.
+  apply encode_not_nil in H. rewrite H. trivial.
+- split; intros. discriminate. 
 
 Theorem encode_decode_inv:
   forall bs vs, decode bs = Some vs <-> encode vs = bs.
 Proof.
-induction bs; simpl; intros.
+induction bs; unfold decode; simpl; intros.
 - (* encoded to nil *)
-unfold decode. simpl.
 split; intros. injection H; intros; subst. simpl. trivial.
+apply encode_not_nil in H. rewrite H. trivial.
+- (* encoded to not nil *)
+unfold encode. 
+split. 
+
 
 
 Qed.
 
 End CODE.
-
-End PREFIX.
 
 Section CONSTLENGTH.
 
