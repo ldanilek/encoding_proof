c3ba7897998f513e989b423253107ce0308d24b3
yichaolemon <yclemontree0@gmail.com>
2021-06-20 19:42:25 -0700

    prefix proof, and some progress on encode_decode proof

--- Prefix.v
+++ Prefix.v
@@ -12,6 +12,8 @@ Section PREFIX.
 
 Definition B: Type := list bool.
 
+(* note all of this prefix stuff doesn't require B to be a list of bool;
+it could be any type with decidable equality. *)
 (* is b1 a prefix of b2 *)
 Fixpoint is_prefix_bool (b1 b2: B): bool :=
   match b1 with
@@ -82,15 +84,102 @@ Proof.
 induction b; autounfold; simpl; intros; trivial.
 discriminate.
 Qed.
+
 (*
-Lemma is_prefix_append: forall p b f, is_prefix p (b ++ (f::nil)) ->
+Lemma list_induction_append: forall T P,
+  P nil ->
+  (forall l (v: T), P l -> P (l ++ (v::nil))) ->
+  (forall l, P l).
+Proof.
+Qed.*)
+
+(*
+Lemma nonempty_append: forall T l (v: T),
+  exists s, cons v l = (firstn (length l) (cons v l)) ++ s::nil.
+Proof.
+induction l; simpl; intros.
+- exists v. reflexivity.
+-  
+
+Qed.*)
+
+Lemma nonzero_length: forall T (l: list T),
+  l <> nil <-> 1 <= length l.
+Proof.
+destruct l; simpl.
+- split; intro. contradiction. omega.
+- split; intro. omega. intro. discriminate.
+Qed.
+
+Lemma apply_both_sides: forall T U (f: T->U) a b,
+  a = b -> f a = f b.
+Proof.
+intros. rewrite H. reflexivity.
+Qed.
+
+Lemma removelast_both_sides: forall T (a: list T) b,
+  a = b -> removelast a = removelast b.
+Proof.
+intros. apply apply_both_sides. assumption.
+Qed.
+
+Lemma length_both_sides: forall T (a: list T) b,
+  a = b -> length a = length b.
+Proof.
+intros. apply apply_both_sides. assumption.
+Qed.
+
+(* docs say this is in the List library, but it's not there.
+so we prove it.
+*)
+Lemma removelast_last:
+  forall T l (a: T), removelast (l ++ a::nil) = l.
+Proof.
+induction l; simpl; intros.
+reflexivity.
+remember (l ++ a0::nil) as l0. destruct l0.
+- apply length_both_sides in Heql0.
+rewrite app_length in Heql0. simpl in Heql0. omega.
+-
+apply removelast_both_sides in Heql0.
+rewrite IHl in Heql0. rewrite Heql0. reflexivity.
+Qed.
+
+Lemma append_nonempty_prefix: forall p b x f,
+  p ++ x = b ++ (f::nil) ->
+  1 <= length x ->
+  is_prefix p b.
+Proof.
+intros. apply is_prefix_app.
+apply nonzero_length in H0.
+destruct (exists_last H0). destruct s.
+subst x.
+exists x0.
+apply removelast_both_sides in H.
+rewrite app_assoc in H.
+repeat rewrite removelast_last in H.
+assumption.
+Qed.
+
+(*
+it's true but hard to prove
+Lemma prefix_length: forall p b x y,
+  p ++ x = b ++ y ->
+  length y <= length x ->
+  is_prefix p b.
+Proof.
+*)
+
+Lemma is_prefix_append: forall p b f,
+  is_prefix p (b ++ (f::nil)) ->
   is_prefix p b \/ p = b ++ (f::nil).
 Proof.
 intros. apply is_prefix_app in H. 
 destruct H.
 rewrite <- H. destruct x.
 - right. rewrite app_nil_r. reflexivity.
-- left. apply is_prefix_spec.
+- left. apply append_nonempty_prefix in H. assumption.
+simpl. omega.
 Qed.
-*)
+
 End PREFIX.
--- PrefixCode.v
+++ PrefixCode.v
@@ -6,10 +6,22 @@ Require Import List.
 Require Import Compare_dec.
 Require Import Omega.
 Require Import Bool.
+Require Import Logic.
 
 (* local import *)
 Require Import Prefix.
+
 Set Implicit Arguments.
+
+Definition equal_some (T: Type) (x: option T) (y: T): Prop :=
+  x = Some y.
+
+Lemma none_dec: forall T (x: option T),
+  {x = None} + {True}.
+Proof.
+destruct x. right. trivial.
+left. reflexivity.
+Defined.
 
 Section CODE.
 
@@ -59,10 +71,11 @@ Definition encode (vs: list V): B :=
 
 Definition is_unmatched_prefix (unmatched: B) :=
   forall p, is_prefix p unmatched -> d p = None.
+Hint Unfold is_unmatched_prefix.
 
 Lemma nil_is_unmatched: is_unmatched_prefix nil.
 Proof.
-unfold is_unmatched_prefix; intros; simpl.
+autounfold; intros; simpl.
 apply is_nil_prefix in H. rewrite H.
 unfold is_prefix in H. unfold is_prefix_bool in H.
 remember (d nil) as v. destruct v; trivial.
@@ -75,9 +88,14 @@ Lemma append_unmatched: forall unmatched f,
   d (unmatched ++ (f::nil)) = None ->
   is_unmatched_prefix (unmatched ++ (f::nil)).
 Proof.
-intros. unfold is_unmatched_prefix.
-
+autounfold.
+intros.
+apply is_prefix_append in H1.
+destruct H1. apply H. assumption.
+subst p. assumption.
 Qed.
+
+
 
 (* decodes unmatched++b into list of values *)
 Fixpoint decode_helper (unmatched b: B) 
@@ -91,8 +109,13 @@ Fixpoint decode_helper (unmatched b: B)
   | f::r =>
     let pref := unmatched ++ (f::nil) in
       match d pref with
-      | None => decode_helper pref r
-      | Some v => match decode_helper nil r proof_ with
+      | None => match none_dec (d pref) with
+        | left pref_no_decode =>
+          @decode_helper pref r
+          (@append_unmatched unmatched f proof_unmatched pref_no_decode)
+        | right _ => None (* unreachable *)
+        end
+      | Some v => match @decode_helper nil r nil_is_unmatched with
         | None => None
         | Some vs => Some (v :: vs)
         end
@@ -100,7 +123,7 @@ Fixpoint decode_helper (unmatched b: B)
   end.
 
 Definition decode (b: B): option (list V) :=
-  decode_helper nil b.
+  @decode_helper nil b nil_is_unmatched.
 
 Lemma encode_not_nil:
   forall x, encode x = nil -> x = nil.
@@ -112,14 +135,78 @@ apply nonempty_code in H.
 contradiction.
 Qed.
 
+Lemma encode_not_unmatched:
+  forall vs unmatched,
+  is_unmatched_prefix unmatched ->
+  encode vs = unmatched ->
+  unmatched = nil.
+Proof.
+induction vs; simpl; intros.
+- symmetry. assumption.
+- exfalso. subst unmatched. autounfold in H.
+cut (d (c a) = None).
++ intro. remember (c a) as b. symmetry in Heqb.
+  apply c_d_inv in Heqb. rewrite Heqb in H0. discriminate.
++ apply H. apply is_prefix_app. exists (encode vs).
+reflexivity.
+Qed.
+
+Lemma list_split: forall T (b: T) c,
+  b :: c = (b :: nil) ++ c.
+Proof.
+intros.
+rewrite <- app_comm_cons.
+simpl. reflexivity.
+Qed.
+
+Lemma list_app_split: forall T a (b: T) c,
+  a ++ b :: c = (a ++ (b::nil)) ++ c.
+Proof.
+intros. rewrite list_split. rewrite app_assoc. reflexivity.
+Qed.
+
 Lemma decode_helper_inv:
-  forall b unmatched vs, decode_helper unmatched b = Some vs 
+  forall b unmatched vs is_unmatched,
+  @decode_helper unmatched b is_unmatched = Some vs
   <-> encode vs = unmatched ++ b.
 Proof.
-induction b; induction unmatched; simpl; intros.
-- split; intros. injection H; intros; subst. simpl. trivial.
+induction b.
+- (* b empty *)
+  simpl. destruct unmatched; intros.
+  * (* unmatched empty *)
+  split; intros. injection H; intros; subst. simpl. trivial.
   apply encode_not_nil in H. rewrite H. trivial.
-- split; intros. discriminate. 
+  * (* b empty, unmatched nonempty *)
+  split; intros. discriminate. exfalso.
+  rewrite app_nil_r in H.
+  cut (b::unmatched = nil).
+    + intro. discriminate.
+    + eapply encode_not_unmatched; eauto.
+- (* b nonempty *)
+  intros.
+  rewrite list_app_split.
+  remember (d (unmatched ++ a::nil)) as d_pref.
+  simpl.
+  destruct d_pref.
+  + (* unmatched++a::nil can be decoded *)
+    (* rewrite once *)
+    pattern (d (unmatched++a::nil)) at 1. rewrite <- Heqd_pref.
+    split; intro.
+    * (* valid decode -> valid encode *)
+      remember (decode_helper b nil_is_unmatched) as decode_rest.
+      destruct decode_rest. injection H; intros; subst.
+      simpl. symmetry in Heqd_pref. apply c_d_inv in Heqd_pref.
+      replace (encode l) with b. rewrite Heqd_pref. reflexivity.
+      symmetry. replace b with (nil++b). eapply IHb.
+      symmetry. eapply Heqdecode_rest.
+      simpl. reflexivity.
+      discriminate.
+    * (* valid encode -> valid decode *)
+      
+  
+  
+  
+Qed.
 
 Theorem encode_decode_inv:
   forall bs vs, decode bs = Some vs <-> encode vs = bs.
